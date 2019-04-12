7e8979cfb456c7ccf326eefb18f60847145dcfd3
Nicholas Latyshov <n.latishov@leviossa.net>
2019-04-12 15:02:14 +0300

    HOTFIX(localstorage issue)

--- js/domEvents.js
+++ js/domEvents.js
@@ -5,66 +5,67 @@ $(document).ready(() => {
     } else {
         return false
     }
+});
 
-    $newTodoButton.bind("click", () => {
+
+$newTodoButton.bind("click", () => {
+    addTodo();
+});
+
+$newTodo.keyup(() => {
+    if(event.keyCode === 13) {
         addTodo();
-    });
+    }
+});
 
-    $newTodo.keyup(() => {
-        if(event.keyCode === 13) {
-            addTodo();
+$body.on('change', '.toggle', function() {
+    let currentID = $(this).parents('li').attr('id');
+    todosArray.forEach((el) => {
+        if (el.id == currentID) {
+            el.checked = this.checked;
         }
     });
+    pagination();
+});
 
-    $body.on('change', '.toggle', function() {
-        let currentID = $(this).parents('li').attr('id');
-        todosArray.forEach((el) => {
-            if (el.id == currentID) {
-                el.checked = this.checked;
-            }
-        });
-        pagination();
+$body.on('click', '.destroy', function() {
+    const currentID = $(this).parents('li').attr('id');
+    todosArray.forEach((el, index) => {
+        if (el.id == currentID) {
+            todosArray.splice(index, 1);
+        }
     });
+    pagination();
+});
 
-    $body.on('click', '.destroy', function() {
-        const currentID = $(this).parents('li').attr('id');
-        todosArray.forEach((el, index) => {
-            if (el.id == currentID) {
-                todosArray.splice(index, 1);
-            }
-        });
-        pagination();
-    });
-
-    $body.on('dblclick', '.todo-label', function() {
-        let originalText = $(this).text();
-        $(this)
-                .addClass("editing")
-                .html
-                    (`<input type="text" id="edit" value="${originalText}"/>`)
-                .children()
-                .focus();
-        const   currentID = $(this).parents('li').attr('id');
-        let     newText = $(this).text();
-        $(this).children().keyup(function (){
-            if (event.keyCode === 13) {
-                editTodo(newText, currentID);
-            }
-        });
-        $(this).children().first().blur(function (){
+$body.on('dblclick', '.todo-label', function() {
+    let originalText = $(this).text();
+    $(this)
+        .addClass("editing")
+        .html
+        (`<input type="text" id="edit" value="${originalText}"/>`)
+        .children()
+        .focus();
+    const   currentID = $(this).parents('li').attr('id');
+    let     newText = $(this).text();
+    $(this).children().keyup(function (){
+        if (event.keyCode === 13) {
             editTodo(newText, currentID);
-        });
+        }
     });
+    $(this).children().first().blur(function (){
+        editTodo(newText, currentID);
+    });
+});
 
-    $body.on('click', '#pages .page', function() {
-        $('#pages .page').removeClass('active');
-        $(this).addClass('active');
-        pagination();
-    });
+$body.on('click', '#pages .page', function() {
+    $('#pages .page').removeClass('active');
+    $(this).addClass('active');
+    pagination();
+});
 
-    $tab.click(function(){
-        $tab.removeClass('active');
-        $(this).addClass('active');
-        pagination('first');
-    });
+$tab.click(function(){
+    $tab.removeClass('active');
+    $(this).addClass('active');
+    pagination('first');
 });
